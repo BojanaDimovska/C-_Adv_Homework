4784c0025cdc0cfa29d2a4c4da09ac2810ce5d76
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-05-31 18:14:04 +0200

    Add files via upload

---
+++ homework4/Task1.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Task1.Domain;
+using Task1.Domain.Classes;
+
+namespace Task1.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericDb<Shape>.Shapes.Add(new Circle() { Id = 1, Radius = 3 });
+            GenericDb<Shape>.Shapes.Add(new Rectangle() { Id = 2, SideA = 2, SideB = 2.5 });
+
+            Console.WriteLine("Area of the shapes:");
+            GenericDb<Shape>.PrintAreas();
+
+            Console.WriteLine("Perimeter of the shapes:");
+            GenericDb<Shape>.PrintPerimeters();
+
+
+            Shape circle = GenericDb<Shape>.Shapes[0];
+            circle.PrintInfo();
+
+            Shape rectangle = GenericDb<Shape>.Shapes[1];
+            rectangle.PrintInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
