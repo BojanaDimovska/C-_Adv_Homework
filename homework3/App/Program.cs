37b644d524a3a2a56259867781e4aa09b91400c4
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-05-31 18:04:28 +0200

    Add files via upload

---
+++ homework3/App/Program.cs
@@ -0,0 +1,23 @@
+﻿using App.Domain;
+using App.Domain.Classes;
+using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (Vehicle vehicle in VehicleDb.Vehicles)
+            {
+                vehicle.PrintVehicle();
+            }
+
+            Console.WriteLine(Validator.Validate(VehicleDb.Vehicles[0]));
+            Console.WriteLine(Validator.Validate(VehicleDb.Vehicles[3]));
+            Console.WriteLine(Validator.Validate(VehicleDb.Vehicles[5]));
+
+            Console.ReadLine();
+        }
+    }
+}
