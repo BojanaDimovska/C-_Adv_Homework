24a4530e465b7f3277ccef09585d029808022700
BojanaDimovska <75226178+BojanaDimovska@users.noreply.github.com>
2021-06-05 21:26:10 +0200

    Add files via upload

---
+++ homework6/Solution1/Task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appPath = @"..\..\..\Exercise";
+            string filePath = appPath + @"\calculations.txt";
+
+            if (!Directory.Exists(appPath))
+            {
+                Directory.CreateDirectory(appPath);
+            }
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+            using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+            {
+                streamWriter.WriteLine(ActionTaker());
+            }
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                string readResult = streamReader.ReadToEnd();
+                Console.WriteLine(readResult);
+            }
+
+            Console.WriteLine("Press ENTER to exit");
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static string Calculate(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            string result = $"{num1} + {num2} = {sum}\n" +
+                $"";
+            return result;
+        }
+        public static string ActionTaker()
+        {
+            string result = "";
+            int i = 1;
+            while (i <= 3)
+            {
+                Console.WriteLine("Enter the first number:\n");
+                bool number1 = int.TryParse(Console.ReadLine(), out int num1);
+                Console.WriteLine("Enter the second number:\n");
+                bool number2 = int.TryParse(Console.ReadLine(), out int num2);
+
+                if (number1 && number2)
+                {
+                    result += Calculate(num1, num2);
+                }
+
+                i++;
+            }
+            return result;
+        }
+    }
+}
